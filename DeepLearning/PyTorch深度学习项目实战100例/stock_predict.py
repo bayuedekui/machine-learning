c91a1faca809783a4ea61c6988fb5915399ac50d
zhengkui <468360786@qq.com>
2023-02-23 23:17:58 +0800

    规整目录

--- DeepLearning/PyTorch深度学习项目实战100例/stock_predict.py
+++ DeepLearning/PyTorch深度学习项目实战100例/stock_predict.py
@@ -10,13 +10,11 @@ def get_data():
     # 调用tushare pro接口获取股票数据
     pro = ts.pro_api("c6b32ba214d901150d6fc0066c63d199f4fe94078202d605a0738134")
 
-    df = pro.daily(ts_code='000001.SZ', start_date='20180701', end_date='20220718')
+    df = pro.daily(ts_code='000001.SZ', start_date='20130101', end_date='20230201')
 
     print(df.info())
+    df.to_csv('inputs/stock_info.csv', index=False)
 
 
 if __name__ == '__main__':
     get_data()
-
-
-
---
+++ DeepLearning/PyTorch深度学习项目实战100例/wind_predict_lstm_cnn.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+from torch.utils.data import TensorDataset,DataLoader
+import matplotlib.pyplot as plt
+
+
